bb100fd3050ca56790bec5d1123b9f5adb8b34e7
Dean Mckendry <dean.mckendry91@gmail.com>
2019-11-12 14:12:09 +1100

    Add create command

--- tasks.py
+++ tasks.py
@@ -2,10 +2,29 @@ import os
 
 from invoke import run, task
 
+PRINCE_FILENAME = 'prince-12.5-linux-generic-x86_64'
+ZIP_FILENAME = 'html2pdf.zip'
+
 
 def prompt(message):
     print(message)
     return input()
+
+
+def create_zip():
+    # Download binary if it doesn't exist
+    if not os.path.exists(f'{PRINCE_FILENAME}.tar.gz'):
+        print('Downloading princeXML...')
+        run(f'curl -O -J https://www.princexml.com/download/{PRINCE_FILENAME}.tar.gz')
+    print('Extracting...')
+    run(f'tar -xf {PRINCE_FILENAME}.tar.gz')
+    print('Copying...')
+    run(f'cp -r {PRINCE_FILENAME}/lib/prince/ html2pdf/')
+    print('Cleaning up...')
+    run(f'rm -r {PRINCE_FILENAME}')
+    # Zip up project contents
+    print('Compressing project...')
+    run(f'cd html2pdf && zip -FSrq ../{ZIP_FILENAME} *')
 
 
 @task
@@ -24,11 +43,13 @@ def setup(ctx):
 def deploy(ctx):
     function_arn = os.getenv('FUNCTION_ARN')
     bucket_name = os.getenv('BUCKET_NAME')
-    # Zip up project contents
-    run(f'cd html2pdf && zip -FSr ../html2pdf.zip *')
+    create_zip()
     # Send to aws
-    run(f'aws lambda update-function-code --function-name {function_arn} --zip-file fileb://html2pdf.zip')
-    run(f'aws s3 cp html2pdf.zip s3://{bucket_name}/html2pdf.zip')
+    print('Updating lambda...')
+    run(f'aws lambda update-function-code --function-name {function_arn} --zip-file fileb://{ZIP_FILENAME}')
+    print('Uploading to s3...')
+    run(f'aws s3 cp {ZIP_FILENAME} s3://{bucket_name}/{ZIP_FILENAME}')
+    print('Done!')
 
 
 @task
@@ -39,3 +60,23 @@ def invoke(ctx):
     print('response:')
     run(f'cat test.json && rm test.json')
     print()
+
+
+@task
+def create(ctx):
+    runtime = 'python3.7'
+    handler = 'lambda_function.lambda_handler'
+    function_name = prompt('function name:')
+    role = prompt('role:')
+    create_zip()
+    command = (
+        f'aws lambda create-function '
+        f'--function-name {function_name} '
+        f'--runtime {runtime} '
+        f'--role {role} '
+        f'--handler {handler} '
+        f'--zip-file fileb://{ZIP_FILENAME} '
+    )
+    print(command)
+    # TODO: Print command and ask user to confirm
+    run(command)
