e0b35318d01c9613d34a71b30b37c4975ffd28ec
Dean Mckendry <dean.mckendry91@gmail.com>
2019-11-15 08:50:54 +1100

    Improve lambda function

--- splat/lambda_function.py
+++ splat/lambda_function.py
@@ -4,77 +4,71 @@ import boto3
 import base64
 
 
-def pdf_from_html(body_html, javascript=True):
-    """
-    Uses princexml to render a pdf from html/css/js
-    Saves the file at /tmp/output.pdf and returns any stdout from prince.
-    """
-    # Save body_html to file
+def pdf_from_string(document_content, javascript=True):
+    # Save document_content to file
     with open('/tmp/input.html', 'w') as f:
-        f.write(body_html)
-    # Run the prince binary and just see if we get some output
-    command = ['./prince/lib/prince/bin/prince', '/tmp/input.html', '-o', '/tmp/output.pdf']
+        f.write(document_content)
+    return prince_handler('/tmp/input.html')
+
+
+def pdf_from_url(document_url, javascript=True):
+    # Fetch URL and save to file
+    raise NotImplementedError()
+
+
+def prince_handler(input_filepath, output_filepath='/tmp/output.pdf', javascript=True,):
+    command = ['./prince/lib/prince/bin/prince', input_filepath, '-o', output_filepath]
     if javascript:
         command.append('--javascript')
     popen = subprocess.Popen(command, stdout=subprocess.PIPE)
     popen.wait()
-    return popen.stdout.read().decode()
+    # TODO: Handle prince errors, stdout, stderr, catch exceptions
+    # stdout = popen.stdout.read().decode()
+    return output_filepath
 
 
+# Entrypoint for AWS
 def lambda_handler(event, context):
-    # Extract html from event
-    body_html = event.get('body_html')
     javascript = bool(event.get('javascript', True))
-    if not body_html:
+    # Create PDF
+    if event.get('document_content'):
+        output_filepath = pdf_from_string(event.get('document_content'), javascript)
+    elif event.get('document_url'):
+        output_filepath = pdf_from_url(event.get('document_url'), javascript)
+    else:
         return {
             'statusCode': 400,
             'headers': {
                 'content-type': 'application/json',
             },
-            'body': json.dumps({'errors': ['Please specify body_html'], 'event': event}),
+            'body': json.dumps({'errors': ['Please specify either "document_content" or "document_url"']}),
             'isBase64Encoded': False,
         }
-    output = pdf_from_html(body_html, javascript)
-    if output:
+
+    # Return PDF
+    if event.get('bucket_name'):
+        # Upload to s3 and return URL
+        bucket_name = event.get('bucket_name')
+        key = 'output.pdf'
+        s3 = boto3.resource('s3')
+        bucket = s3.Bucket(bucket_name)
+        bucket.upload_file('/tmp/output.pdf', key)
+        location = boto3.client('s3').get_bucket_location(Bucket=bucket_name)['LocationConstraint']
+        url = f'https://{bucket_name}.s3-{location}.amazonaws.com/{key}'
+
         return {
-            'statusCode': 500,
+            'statusCode': 200,
             'headers': {
                 'content-type': 'application/json',
             },
-            'body': json.dumps({'errors': [output]}),
+            'body': json.dumps({'url': url}),
             'isBase64Encoded': False,
         }
+    elif event.get('presigned_url'):
+        raise NotImplementedError()
     else:
-        # Choose a method for uploading, either direct or presigned
-        if event.get('bucket_name'):
-            # Upload to s3 and return URL
-            bucket_name = event.get('bucket_name')
-            key = 'output.pdf'
-            s3 = boto3.resource('s3')
-            bucket = s3.Bucket(bucket_name)
-            bucket.upload_file('/tmp/output.pdf', key)
-            location = boto3.client('s3').get_bucket_location(Bucket=bucket_name)['LocationConstraint']
-            url = f'https://{bucket_name}.s3-{location}.amazonaws.com/{key}'
+        # Otherwise just stream the pdf data back.
+        with open(output_filepath, 'rb') as f:
+            binary_data = f.read()
 
-            return {
-                'statusCode': 200,
-                'headers': {
-                    'content-type': 'application/json',
-                },
-                'body': json.dumps({'url': url}),
-                'isBase64Encoded': False,
-            }
-        elif event.get('presigned_url'):
-            presigned_url = event.get('presigned_url')
-            raise NotImplementedError()
-        else:
-            # Otherwise just stream the pdf data back.
-            with open('/tmp/output.pdf', 'rb') as f:
-                binary_data = f.read()
-
-            return base64.b64encode(binary_data).decode()
-
-    return {
-        'statusCode': 200,
-        's3_object_url': url,
-    }
+        return base64.b64encode(binary_data).decode()
