093be527e7a6f40c525fd949de52f65f7ba6cd56
Dean Mckendry <dean.mckendry91@gmail.com>
2020-01-24 08:04:33 +1100

    Covert install script to boto3

--- tasks.py
+++ tasks.py
@@ -1,7 +1,9 @@
 import json
 import os
-import time
+from pprint import pprint
+from time import sleep
 
+import boto3
 from invoke import run, task
 from invoke.exceptions import UnexpectedExit
 
@@ -49,171 +51,133 @@ def deploy(ctx):
     create_zip()
     # Send to aws
     print('Updating lambda...')
-    run(f'aws lambda update-function-code --function-name {FUNCTION_NAME} --zip-file fileb://{ZIP_FILENAME}')
+    lambda_client = boto3.client('lambda')
+    with open(ZIP_FILENAME, 'rb') as zipfile:
+        response = lambda_client.update_function_code(
+            FunctionName=FUNCTION_NAME,
+            ZipFile=zipfile.read(),
+        )
     print('Done!')
+    pprint(response)
 
 
 @task
 def create(ctx):
-    # TODO: boto3
+    # TODO: Print AWS region info + confirm?
     create_zip()
-    # Create role
-    command = (
-        'aws iam create-role '
-        f'--role-name {FUNCTION_NAME}-role '
-        '--assume-role-policy-document \'{"Version": "2012-10-17", "Statement": [{"Effect": "Allow", "Principal": {"Service": "lambda.amazonaws.com"}, "Action": "sts:AssumeRole"}]}\''
-    )
-    output = run_aws_command(command)
-    role_arn = output['Role']['Arn']
-    role_name = output['Role']['RoleName']
 
-    # Attach policy to role
-    command = (
-        'aws iam attach-role-policy '
-        f'--role-name {role_name} '
-        '--policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole '
-    )
-    run_aws_command(command, output=False)
+    print("Obtaining clients...")
+    iam_client = boto3.client('iam')
+    lambda_client = boto3.client('lambda')
+    apigateway_client = boto3.client('apigateway')
 
-    # It can take a few seconds for AWS to replicate the role through all regions, so the lambda creation can fail
-    # Just keep trying until it goes through.
-
-    # Create lambda
-    command = (
-        'aws lambda create-function '
-        f'--function-name {FUNCTION_NAME} '
-        f'--runtime python3.7 '
-        f'--role {role_arn} '
-        f'--handler lambda_function.lambda_handler '
-        f'--zip-file fileb://{ZIP_FILENAME} '
+    print("Creating role...")
+    result = iam_client.create_role(
+        RoleName=f'{FUNCTION_NAME}-role',
+        AssumeRolePolicyDocument='{"Version": "2012-10-17", "Statement": [{"Effect": "Allow", "Principal": {"Service": "lambda.amazonaws.com"}, "Action": "sts:AssumeRole"}]}',
     )
-    lambda_created = False
+    role_arn = result['Role']['Arn']
+    role_name = result['Role']['RoleName']
+
+    print("Attaching policy to role...")
+    result = iam_client.attach_role_policy(
+        RoleName=role_name,
+        PolicyArn='arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole',
+    )
+
+    print("Creating lambda...")
     attempts = 0
-    while not lambda_created:
+    while True:
         try:
-            output = run_aws_command(command)
-            lambda_created = True
-        except UnexpectedExit as e:
-            print('lambda creation failed, waiting on role to be available...')
+            with open(ZIP_FILENAME, 'rb') as zipfile:
+                result = lambda_client.create_function(
+                    FunctionName=FUNCTION_NAME,
+                    Runtime='python3.8',
+                    Role=role_arn,
+                    Handler='lambda_function.lambda_handler',
+                    Code={'ZipFile': zipfile.read()},
+                )
+            break
+        except lambda_client.exceptions.InvalidParameterValueException:
             attempts += 1
-            if attempts >= 5:
-                raise Exception(f"Lambda creation failed: {e}")
-            time.sleep(3)
+            print("Failed. Probably role replication. Trying again.")
+            if attempts > 5:
+                raise Exception(f"Failed to create lambda after {attempts} attempts. The role takes a while to replicate, but not usually this long...")
+            sleep(5)
 
-    lambda_arn = output['FunctionArn']
+    lambda_arn = result['FunctionArn']
     region = lambda_arn.split(':')[3]
     account_id = lambda_arn.split(':')[4]
 
-    # Create API
-    command = (
-        'aws apigateway create-rest-api '
-        f'--name {FUNCTION_NAME}-api '
+    print("Creating API...")
+    result = apigateway_client.create_rest_api(
+        name=f'{FUNCTION_NAME}-api',
+        binaryMediaTypes=['application/pdf']
     )
-    output = run_aws_command(command)
-    api_id = output['id']
-    api_root_id = run_aws_command(f'aws apigateway get-resources --rest-api-id {api_id}')['items'][0]['id']
+    api_id = result['id']
 
-    # Create POST method on /
-    command = (
-        'aws apigateway put-method '
-        f'--rest-api-id {api_id} '
-        f'--resource-id {api_root_id} '
-        '--http-method POST '
-        '--authorization-type NONE '
+    print("Getting API root ID...")
+    result = apigateway_client.get_resources(
+        restApiId=api_id,
     )
-    run_aws_command(command)
+    api_root_id = result['items'][0]['id']
 
-    # Set our lambda as the destination of the POST method
+    print("Create post method on /")
+    apigateway_client.put_method(
+        restApiId=api_id,
+        resourceId=api_root_id,
+        httpMethod='POST',
+        authorizationType='NONE',
+        apiKeyRequired=True,
+    )
+
+    print("Set lambda proxy as the integration...")
     uri = f'arn:aws:apigateway:{region}:lambda:path/2015-03-31/functions/arn:aws:lambda:{region}:{account_id}:function:{FUNCTION_NAME}/invocations'
-    command = (
-        'aws apigateway put-integration '
-        f'--rest-api-id {api_id} '
-        f'--resource-id {api_root_id} '
-        '--http-method POST '
-        '--type AWS '
-        '--integration-http-method POST '
-        f'--uri {uri} '
+    apigateway_client.put_integration(
+        restApiId=api_id,
+        resourceId=api_root_id,
+        httpMethod='POST',
+        type='AWS_PROXY',
+        integrationHttpMethod='POST',
+        uri=uri,
     )
-    run_aws_command(command)
 
-    # Set API response type to JSON
-    command = (
-        'aws apigateway put-method-response '
-        f'--rest-api-id {api_id} '
-        f'--resource-id {api_root_id} '
-        '--http-method POST '
-        '--status-code 200 '
-        '--response-models application/json=Empty '
+    print("Deploy the API...")
+    apigateway_client.create_deployment(
+        restApiId=api_id,
+        stageName='prod',
     )
-    run_aws_command(command)
 
-    # Set lambda response type to JSON
-    command = (
-        'aws apigateway put-integration-response '
-        f'--rest-api-id {api_id} '
-        f'--resource-id {api_root_id} '
-        '--http-method POST '
-        '--status-code 200 '
-        '--response-templates application/json="" '
-        '--content-handling CONVERT_TO_BINARY '
+    print("Create usage plan...")
+    result = apigateway_client.create_usage_plan(
+        name=f'{FUNCTION_NAME}-usageplan',
+        apiStages=[{'apiId': api_id, 'stage': 'prod'}]
     )
-    run_aws_command(command)
+    usage_plan_id = result['id']
 
-    # Deploy the API
-    command = (
-        'aws apigateway create-deployment '
-        f'--rest-api-id {api_id} '
-        '--stage-name prod '
+    print("Create API key...")
+    result = apigateway_client.create_api_key(
+        name=f'{FUNCTION_NAME}-key',
+        enabled=True,
     )
-    run_aws_command(command)
+    api_key = result['value']
+    api_key_id = result['id']
 
-    # Create usage plan
-    command = (
-        'aws apigateway create-usage-plan '
-        f'--name {FUNCTION_NAME}-usageplan '
-        f'--api-stages apiId={api_id},stage=prod '
+    print("Enable the API key for the usage plan...")
+    apigateway_client.create_usage_plan_key(
+        usagePlanId=usage_plan_id,
+        keyId=api_key_id,
+        keyType='API_KEY',
     )
-    output = run_aws_command(command)
-    usage_plan_id = output['id']
 
-    # Create API key
-    command = (
-        'aws apigateway create-api-key '
-        f'--name {FUNCTION_NAME}-key '
-        '--enabled '
+    print("Grant invoke permissions to the API")
+    lambda_client.add_permission(
+        FunctionName=FUNCTION_NAME,
+        StatementId=f'{FUNCTION_NAME}-prod',
+        Action='lambda:InvokeFunction',
+        Principal='apigateway.amazonaws.com',
+        SourceArn=f'arn:aws:execute-api:{region}:{account_id}:{api_id}/*/POST/',
     )
-    output = run_aws_command(command)
-    api_key = output['value']
-    api_key_id = output['id']
-
-    # Enable the API key for the usage plan
-    command = (
-        'aws apigateway create-usage-plan-key '
-        f'--usage-plan-id {usage_plan_id} '
-        f'--key-id {api_key_id} '
-        '--key-type API_KEY '
-    )
-    run_aws_command(command)
-
-    # Grant invoke permissions to the API
-    command = (
-        'aws lambda add-permission '
-        f'--function-name {FUNCTION_NAME} '
-        f'--statement-id {FUNCTION_NAME}-test '
-        '--action lambda:InvokeFunction '
-        '--principal apigateway.amazonaws.com '
-        f'--source-arn "arn:aws:execute-api:{region}:{account_id}:{api_id}/*/POST/" '
-    )
-    run_aws_command(command)
-    command = (
-        'aws lambda add-permission '
-        f'--function-name {FUNCTION_NAME} '
-        f'--statement-id {FUNCTION_NAME}-prod '
-        '--action lambda:InvokeFunction '
-        '--principal apigateway.amazonaws.com '
-        f'--source-arn "arn:aws:execute-api:{region}:{account_id}:{api_id}/prod/POST/" '
-    )
-    run_aws_command(command)
 
     api_url = f'https://{api_id}.execute-api.{region}.amazonaws.com/prod'
 
